0e85ff2079d43cffbcc31502aed7a896b7687d54
Rodrigues <fr.costa95@gmail.com>
2018-06-17 12:55:43 +0000

    model contact changed

--- app/models/contato.rb
+++ app/models/contato.rb
@@ -1,7 +1,7 @@
 class Contato < ActiveRecord::Base
   belongs_to :tipo
-  has_one :endereco
-  has_many :telefones
+  has_one :endereco, dependent: :destroy
+  has_many :telefones, dependent: :destroy
   
   accepts_nested_attributes_for :endereco
   accepts_nested_attributes_for :telefones, reject_if: :all_blank, allow_destroy: true
